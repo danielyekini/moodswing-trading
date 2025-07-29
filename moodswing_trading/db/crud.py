4b34b37992da5ee0dbfc6e09eaa590634b062565
danielyekini <feyiyek@gmail.com>
2025-07-29 22:33:16 +0100

    Refactor get_db function to use a generator for session management

--- moodswing_trading/db/crud.py
+++ moodswing_trading/db/crud.py
@@ -9,7 +9,9 @@ from sqlalchemy.orm import Session
 from .models import SessionLocal, Article, SentimentDay, Prediction
 
 
-def get_db() -> Session:
+from typing import Generator
+
+def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
         yield db
